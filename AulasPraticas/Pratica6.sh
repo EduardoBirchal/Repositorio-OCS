e4c728dadd86862a13b554ad6f1d43e0398b5ef0
EduardoBirchal <104524053+EduardoBirchal@users.noreply.github.com>
2022-06-02 09:16:17 -0300

    Atualizado

--- AulasPraticas/Pratica6.sh
+++ AulasPraticas/Pratica6.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 paralelepipedo () {
-    a=$[$2**2]
-    b=$[$3**2]
-    c=$[$4**2]
+    a=$(echo "scale=2; $1 ^ 2" | bc) 
+    b=$(echo "scale=2; $2 ^ 2" | bc)
+    c=$(echo "scale=2; $3 ^ 2" | bc)
 
-    d=$[$a+$b+$c]
-    echo "scale=3; sqrt($d)" | bc
+    d=$(echo "scale=2; $a + $b + $c" | bc)
+    echo "scale=2; sqrt($d)" | bc
+}
+
+MDC () {
+    mdc=1
+    res=1
+
+    while [ $mdc -le $1 ] && [ $mdc -le $2 ] && [ $mdc -le $3 ] # O divisor não pode ser maior que o dividendo
+        do
+        if [ $(($1%$mdc)) -eq 0 ] && [ $(($2%$mdc)) -eq 0 ] && [ $(($3%$mdc)) -eq 0 ] # Se todos forem divisíveis, transforma o MDC nesse número
+            then
+            res=$mdc
+        fi
+        mdc=$(($mdc+1))
+        done
+    echo "$res"
 }
 
 case $1 in
-paralelepipedo) paralelepipedo ;;
-*) echo "Opcao invalida" ;;
+    paralelepipedo) paralelepipedo $2 $3 $4;;
+    mdc) MDC $2 $3 $4;;
+    *) echo "Opcao invalida" ;;
 esac
